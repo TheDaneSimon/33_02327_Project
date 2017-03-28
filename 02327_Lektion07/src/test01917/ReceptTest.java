c073c14b3d60fd231e03bedfab48ee5123fb76a4
Mads <mads@stege.eu>
2017-03-28 12:04:41 +0200

    Corrected some errors in Recept-classes

---
+++ 02327_Lektion07/src/test01917/ReceptTest.java
@@ -0,0 +1,36 @@
+package test01917;
+
+import java.sql.SQLException;
+
+import connector01917.Connector;
+import daoimpl01917.MySQLOperatoerDAO;
+import daoimpl01917.MySQLReceptDAO;
+import daointerfaces01917.DALException;
+import dto01917.ReceptDTO;
+
+public class ReceptTest {
+	public static void main(String[] args){
+		try {
+			new Connector();
+		} catch (InstantiationException e){
+			e.printStackTrace();
+		} catch (IllegalAccessException e){
+			e.printStackTrace();
+		} catch (ClassNotFoundException e){
+			e.printStackTrace();
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		MySQLReceptDAO rc = new MySQLReceptDAO();
+		try{
+			rc.createRecept(new ReceptDTO(5, "Spaghetti"));
+			System.out.println(rc.getRecept(5));
+			System.out.println(rc.getReceptList());
+			
+			rc.updateRecept(new ReceptDTO(1, "margherita"));
+			System.out.println(rc.getReceptList());
+		} catch (DALException e){
+			e.printStackTrace();
+		}
+	}
+}
