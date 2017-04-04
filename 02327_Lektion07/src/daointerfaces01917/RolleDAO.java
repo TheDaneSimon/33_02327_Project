a4da33ae36d8e75999c13d2a736ee60b94decd2e
Peter <peterxp@hotmail.fr>
2017-04-04 11:01:59 +0200

    Update RolleDAO

--- 02327_Lektion07/src/daointerfaces01917/RolleDAO.java
+++ 02327_Lektion07/src/daointerfaces01917/RolleDAO.java
@@ -1,7 +1,15 @@
 package daointerfaces01917;
 
+import java.util.List;
+
+import dto01917.RolleDTO;
+
 public interface RolleDAO {
 
-	
+	public List<RolleDTO> getOprRolleList(int opr_id);
+	public List<RolleDTO> getRolleList();
+	public List<RolleDTO> getOprRolleList(String rolle);
+	public void createRolle(RolleDTO rolle);
+	public void updateRolle(RolleDTO rolle);
 	
 }
